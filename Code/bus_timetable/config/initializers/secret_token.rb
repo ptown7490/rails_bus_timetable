879b223ed8ac79f0dcbe0ebc0ee2497d1b981612
Brentwood Reid & Patrick McGreevy <student@epicodus.com>
2014-04-07 09:07:57 -0700

    initial commit

---
+++ Code/bus_timetable/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BusTimetable::Application.config.secret_key_base = 'c766f2467d4ae83c54d939a5e48fd98aa97cc42715528cbcc85b7e5ca8fbf080708de34250612f05515c9ab3b525fcf190bdaff001778b127499e95887e12f53'
